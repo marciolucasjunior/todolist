bb8bb8e5501474cfa338711c55eed728c9d07b03
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-08-07 15:07:32 -0300

    termino do css

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//SELEÇÃO DE ELEMENTOS
+const inputTexto = document.querySelector('#inputTexto');
+const buscar = document.querySelector('#buscar');
+const selected = document.querySelector('#selected');
+const buscar = document.querySelector('#buscar');
+const buscar = document.querySelector('#buscar');
+const buscar = document.querySelector('#buscar');
+
+
+
+
+//FUNÇÕES
+
+
+
+//EVENTOS
